cf2b29879665541d0c6c60ef5df8961eec453309
Brishketu Kislay <brishketu.kislay@gmail.com>
2024-01-12 02:55:29 +0000

    cors fix

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const errorHandler = require('./middleware/errorHandler');
 const connectDb = require('./config/dbConnection');
 const dotenv = require('dotenv').config(); 
+const cors = require('cors');
 connectDb();
 
 const app = express();
 const port = process.env.PORT || 3001;
+app.use(cors,{
+    origin:'*'
+})
 app.use(express.json()); // a parser to accept json from the client
 app.use('/api/contacts', require('./routes/contactsRoute'));
 app.use('/api/users', require('./routes/userRoute'));
