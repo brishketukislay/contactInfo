0328edf582972559ca7866c34eedbf7b200aec85
Brishketu Kislay <brishketu.kislay@gmail.com>
2024-01-12 03:05:28 +0000

    build fix

--- server.js
+++ server.js
@@ -2,19 +2,19 @@ const express = require('express');
 const errorHandler = require('./middleware/errorHandler');
 const connectDb = require('./config/dbConnection');
 const dotenv = require('dotenv').config(); 
-const cors = require('cors');
+// const cors = require('cors');
 connectDb();
 
 const app = express();
 const port = process.env.PORT || 3001;
-app.use(cors,{
-    origin:'*'
-})
+// app.use(cors,{
+//     origin:'*'
+// })
 app.use(express.json()); // a parser to accept json from the client
 app.use('/api/contacts', require('./routes/contactsRoute'));
 app.use('/api/users', require('./routes/userRoute'));
 app.use(errorHandler);
 
-app.listen(3000, ()=>{
-    console.log("server running at", 3000);
+app.listen(port, ()=>{
+    console.log("server running at", port);
 })
